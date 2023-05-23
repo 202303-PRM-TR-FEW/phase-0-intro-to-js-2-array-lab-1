eae17bfe2f971dab81e8d009a2bb8db185a351a6
Izdihar <ezdiharomerrrr@gmail.com>
2023-05-23 11:06:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+cats.push("Ralph");
+
+console.log(cats);
+
+
+
+cats.unshift("Bob");
+
+console.log(cats);
+
+
+cats.pop();
+console.log(cats);
+
+
+
+cats.shift();
+console.log(cats);
+
+
+
+
+function appendCat(name) {
+   
+    const newCats = [...cats, name];
+    return newCats;
+  }
+
+ 
+
+  function removeLastCat() {
+    const newcats2 = [...cats];
+    newcats2.pop();  
+    return newcats2;
+  }
+  
+ 
+
+
+  function removeFirstCat() {
+    const newcats2 = [...cats];
+    newcats2.shift();  
+    return newcats2;
+  }
+
+  function destructivelyAppendCat(name) {
+   
+    cats.push(name);
+  }
+  
+  function destructivelyPrependCat(name) {
+   
+    cats.unshift(name);
+  }
+  
+  function destructivelyRemoveLastCat() {
+   
+    cats.pop();
+  }
+  
+  function destructivelyRemoveFirstCat() {
+ 
+    cats.shift();
+  }
+  
+  function prependCat(name) {
+ 
+    const newCats = [name, ...cats];
+    return newCats;
+  }
+  
